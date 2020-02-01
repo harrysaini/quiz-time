44bb31a96b4011f8dc1cd0ed9f9cde2c5486b611
aashish agrawal <aagarwal063@gmail.com>
2020-02-01 16:45:14 +0530

    prettier file added

--- server/src/dao/user.dao.ts
+++ server/src/dao/user.dao.ts
@@ -1,17 +1,22 @@
-import { IUser } from '../models/user.model';
-import DAO from '../factories/dao';
-import QueryExecutor from '../utils/database/queryExecutor';
+import { IUser } from '../models/user.model'
+import DAO from '../factories/dao'
+import QueryExecutor from '../utils/database/queryExecutor'
 
 class UserDAO extends DAO {
+    constructor(tableName: string) {
+        super(tableName)
+    }
 
-  constructor(tableName: string) {
-    super(tableName);
-  }
-
-  async create(userObj: IUser) {
-    const query = `INSERT INTO \`${this.tableName}\` (\`id\`, \`name\`, \`username\`, \`salt\`, \`password\`) VALUES( ?, ?, ?, ?, ? )`;
-    await QueryExecutor.preparedQuery(query, [userObj.id, userObj.name, userObj.username, userObj.salt, userObj.password]);
-  }
+    async create(userObj: IUser) {
+        const query = `INSERT INTO \`${this.tableName}\` (\`id\`, \`name\`, \`username\`, \`salt\`, \`password\`) VALUES( ?, ?, ?, ?, ? )`
+        await QueryExecutor.preparedQuery(query, [
+            userObj.id,
+            userObj.name,
+            userObj.username,
+            userObj.salt,
+            userObj.password,
+        ])
+    }
 }
 
-export default UserDAO;
+export default UserDAO
